c08a7dbbcd1646c2a3de292416728acd17033e32
dikanp <andika.dikanp@gmail.com>
2016-03-12 18:26:48 +0700

    Added files via upload

---
+++ src/ticketing/Rute.java
@@ -0,0 +1,16 @@
+public class Rute{
+	private Stasiun[] stasiun;
+	private int nStasiun;
+	
+	public Rute(int nStasiun){
+		this.nStasiun=nStasiun;
+	}
+	public addStasiun(Stasiun s){
+	}
+	public getStasiun(int n){
+		return Stasiun[n];
+	}
+	public getDaftarStasiun(){
+		return 
+	}
+}
